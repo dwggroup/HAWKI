b0b26ca184248128c7fd82d46cd17a79e7fa5e23
MarlonKr <marlonmkress@gmail.com>
2024-02-14 15:17:31 +0100

    Update GPT model version to gpt-4-0125-preview

--- interface.php
+++ interface.php
@@ -237,7 +237,7 @@ if (file_exists(".env")){
 <div class="modal" onclick="modalClick(this)" id="gpt4"> 
 	<div class="modal-content">
 		<h2>GPT 3.5</h2>
-		<p>Es wird das Model gpt-3.5-turbo-0125 verwendet.   
+		<p>Es wird das Model gpt-4-0125-preview verwendet.   
 		</p>
 		<button>Bestätigen</button>
 	</div>
